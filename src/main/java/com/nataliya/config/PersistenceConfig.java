099e8cbde4f894199f04852ce2e9c1e541319e04
Nataliya <n.basanina@gmail.com>
2025-07-22 00:42:58 +0300

    feat: add hibernate.properties and PersistenceConfig

---
+++ src/main/java/com/nataliya/config/PersistenceConfig.java
@@ -0,0 +1,59 @@
+package com.nataliya.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@RequiredArgsConstructor
+@PropertySource("classpath:hibernate.properties")
+@EnableTransactionManagement
+public class PersistenceConfig {
+
+    private final Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(env.getRequiredProperty("db.url"));
+        config.setUsername(env.getRequiredProperty("db.username"));
+        config.setPassword(env.getRequiredProperty("db.password"));
+        config.setDriverClassName(env.getRequiredProperty("db.driver_class"));
+        return new HikariDataSource(config);
+    }
+
+    private Properties hibernateProperties(){
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+        properties.setProperty("hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"));
+        return properties;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
+        factory.setDataSource(dataSource());
+        factory.setPackagesToScan("com.nataliya.models");
+        factory.setHibernateProperties(hibernateProperties());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager(){
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+}
