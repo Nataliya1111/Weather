542aabc2cab940829574a6fc1475b0d87df73bde
Nataliya <n.basanina@gmail.com>
2025-08-11 02:02:46 +0300

    feat: create AuthenticationCheckInterceptor, configure it in WebMvcConfig

--- src/main/java/com/nataliya/config/WebMvcConfig.java
+++ src/main/java/com/nataliya/config/WebMvcConfig.java
@@ -1,25 +1,24 @@
 package com.nataliya.config;
 
+import com.nataliya.interceptor.AuthenticationCheckInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Configuration
-@ComponentScan(basePackages = {"com.nataliya.controller", "com.nataliya.handler"})
+@ComponentScan(basePackages = {"com.nataliya.controller", "com.nataliya.handler", "com.nataliya.interceptor"})
 @EnableWebMvc
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
+    private final AuthenticationCheckInterceptor authenticationCheckInterceptor;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -28,6 +27,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("/images/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationCheckInterceptor)
+                .addPathPatterns("/**")      // apply to all pages
+                .excludePathPatterns("/sign-in", "/sign-up", "/css/**", "/images/**"); // exceptions
     }
 
     @Bean
---
+++ src/main/java/com/nataliya/interceptor/AuthenticationCheckInterceptor.java
@@ -0,0 +1,48 @@
+package com.nataliya.interceptor;
+
+import com.nataliya.dto.UserDto;
+import com.nataliya.mapper.UserMapper;
+import com.nataliya.model.Session;
+import com.nataliya.service.SessionService;
+import com.nataliya.util.CookieUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationCheckInterceptor implements HandlerInterceptor {
+
+    private static final String SIGN_IN_VIEW = "sign-in";
+
+    private final SessionService sessionService;
+    private final UserMapper userMapper;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             @NonNull HttpServletResponse response,
+                             @NonNull Object handler) throws Exception {
+
+        Cookie[] cookies = request.getCookies();
+        Optional<Cookie> cookieOptional = CookieUtil.getSessionIdCookie(cookies);
+        if (cookieOptional.isEmpty()) {
+            response.sendRedirect(SIGN_IN_VIEW);
+            return false;
+        }
+        Cookie sessionIdCookie = cookieOptional.get();
+        Session session = sessionService.getSession(UUID.fromString(sessionIdCookie.getValue()));
+        UserDto userDto = userMapper.userToUserDto(session.getUser());
+
+        request.setAttribute("authUserDto", userDto);
+
+        return true;
+    }
+
+}
