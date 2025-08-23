2d1680398e1dae2a96c28deb08e94343602c9479
Nataliya <n.basanina@gmail.com>
2025-08-23 17:44:55 +0300

    refactor: put schema in V1__Init migration as a placeholder, fix misprint in V3__Create_location_table

--- src/main/java/com/nataliya/config/FlywayConfig.java
+++ src/main/java/com/nataliya/config/FlywayConfig.java
@@ -1,18 +1,33 @@
 package com.nataliya.config;
 
 import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class FlywayConfig {
 
+    @Value("${hibernate.default_schema}")
+    private String schema;
+
     @Bean
     public Flyway flyway(DataSource dataSource) {
-        Flyway flyway = Flyway.configure().dataSource(dataSource).defaultSchema("main").load();
+
+        Map<String, String> placeholders = new HashMap<>();
+        placeholders.put("schema", schema);
+
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .defaultSchema(schema)
+                .placeholders(placeholders)
+                .load();
         flyway.migrate();
+
         return flyway;
     }
 }
